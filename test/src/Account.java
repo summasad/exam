ef4eecae1cebf661e0e352e8141c4d50b10c87b4
김현진 <summersad10@gmail.com>
2024-09-13 10:12:15 +0900

    getter/setter 메소드 추가

--- test/src/Account.java
+++ test/src/Account.java
@@ -6,4 +6,24 @@ public class Account {
     public String getAccountNo() {
         return accountNo;
     }
+
+    public int getBalance() {
+        return balance;
+    }
+
+    public String getOwner() {
+        return owner;
+    }
+
+    public void setAccountNo(String accountNo) {
+        this.accountNo = accountNo;
+    }
+
+    public void setBalance(int balance) {
+        this.balance = balance;
+    }
+
+    public void setOwner(String owner) {
+        this.owner = owner;
+    }
 }
